eba47fee4abd8b46839712a7d64ff2b97b6fe2c8
Michael Tarasyuk <michael.tarasyuk91@gmail.com>
2017-07-01 20:13:14 +0300

    Demo forms

--- app/forms/project/create_form.rb
+++ app/forms/project/create_form.rb
@@ -1,5 +1,24 @@
 class Project
   class CreateForm
     # TODO :Implement
+    include ActiveModel::Model
+    include Virtus.model
+
+    attribute :id, Integer
+    attribute :name, String
+    attribute :user_id, Integer
+
+    validates :name, presence: true
+
+    def persist?
+      @record = id ? User.find(id) : User.new
+
+      if @record.valid?
+        @record.save!
+        true
+      else
+        false
+      end
+    end
   end
 end
--- app/forms/project/update_form.rb
+++ app/forms/project/update_form.rb
@@ -1,5 +1,17 @@
 class Project
   class UpdateForm
     # TODO :Implement
+    include ActiveModel::Model
+    include Virtus.model
+
+    attribute :id, Integer
+    attribute :name, String
+    attribute :user_id, Integer
+
+    validates :name, presence: true
+
+    def persist
+
+    end
   end
 end
