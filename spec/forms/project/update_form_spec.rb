765db08187ddebadd98211485095d859a71f9adb
Michael Tarasyuk <michael.tarasyuk91@gmail.com>
2017-07-02 03:40:40 +0300

    Added forms rspec tests

---
+++ spec/forms/project/update_form_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Project::UpdateForm, type: :model do
+  describe 'attributes' do
+    it 'has name' do
+      expect(described_class).to have_attribute(:name).of_type(String)
+    end
+
+    it 'has user_id' do
+      expect(described_class).to have_attribute(:user_id).of_type(Integer)
+    end
+  end
+
+  describe 'validates' do
+    it 'has name' do
+      is_expected.to validate_presence_of(:name)
+    end
+
+    it 'not have user_id' do
+      is_expected.not_to validate_presence_of(:user_id)
+    end
+  end
+end
